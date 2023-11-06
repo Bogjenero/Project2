1378d3df26f895b63847ae42569d6b0625d7f244
Bogjenero <bornagjenero1@gmail.com>
2023-11-06 23:32:50 +0100

    Brisanje1

--- Project2/Zadatak1.cpp
+++ Project2/Zadatak1.cpp
@@ -6,6 +6,6 @@ int main() {
 	std::cout << "" << a + b << std::endl;
 	std::cout << "Ispis" << std::endl;
 	std::cout << "Ispis2" << std::endl;
-	std::cout << "Ispis2" << std::endl;
+	
 	return 0;
 }
